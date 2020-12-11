1cba7dd3f90be959eb6defae6373a04bb5872670
yulyageek <74655056+yulyageek@users.noreply.github.com>
2020-12-11 09:05:49 +0300

    Update Client.c

--- Client.c
+++ Client.c
@@ -10,37 +10,37 @@
 #define path "/home/yulya/kt/FIFO.fifo"
 #define part_path "/home/yulya/kt/"
 #define max_file_size 4096
+#define N 50
 
 int main(int argc, char * argv[]){
+
 	if (argc != 2){
 		printf("arguments error\n");
 		exit(-1);
 	}
 
-	struct stat *about_file = (struct stat *) malloc (sizeof(struct stat));
-	char *data;
-	data = (char *) malloc (50*sizeof(char));
-	char *file;
-	file = (char *) malloc (50*sizeof(char));
-	char * text;
-	text = (char *) malloc (max_file_size * sizeof (char));
+	char *data = (char *) malloc (N*sizeof(char));
+	char *file = (char *) malloc (N*sizeof(char));
+	char * text = (char *) malloc (max_file_size * sizeof (char));
 	int fd_r, fd_w;
 
-	fd_w = open (path, O_WRONLY|O_NONBLOCK);
+	fd_w = open (path, O_WRONLY);
 	if ( fd_w == -1 ){
-		printf("open fifo error\n");
+		printf("open fifo1 error\n");
 		exit (errno);
 	}
+
 	pid_t id;
 	id = getpid();
 	sprintf(data, "%d;%s", id, argv[1]);
-	if ( write(fd_w, data, 50) == -1 ){
+	if ( write(fd_w, data, N) == -1 ){
 		printf("write in fifo error\n");
 		exit (errno);
 	}
 	close(fd_w);
-	char new_path[50];
-	sprintf(new_path, "%s/client%d.fifo", part_path, id);
+
+	char new_path[N];
+	sprintf(new_path, "%sclient%d.fifo", part_path, id);
 
 	if ( mkfifo (new_path, 0600) == -1){
 		if (errno != EEXIST){
@@ -51,7 +51,7 @@ int main(int argc, char * argv[]){
 
 	fd_r = open (new_path, O_RDONLY);
 	if ( fd_r == -1 ){
-		printf("open fifo error\n");
+		printf("open fifo2 error\n");
 		exit (errno);
 	}
 
@@ -61,4 +61,8 @@ int main(int argc, char * argv[]){
 	}
 	close(fd_r);
 	printf("%s", text);
+	free(data);
+	free(file);
+	free(text);
 }
+
